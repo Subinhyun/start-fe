84bdccc11933ca072b979bfa4c8de561d80aa841
hkhh972123 <hkhh972123@gmail.com>
2022-04-15 00:06:53 +0900

    fix random number

--- 07/quiz8/quiz8.js
+++ 07/quiz8/quiz8.js
@@ -7,8 +7,10 @@ const $btn = document.getElementById('btn');
 let num = 0;
 
 $btn.addEventListener('click', () => {
-    const randomNumber = Math.floor(Math.random() * ($max.value - $min.value)) + $min.value;
-
+    const randomNumber = Math.floor(Math.random() * ($max.value - $min.value) + $min.value);
+    console.log($min.value);
+    console.log($max.value);
+    console.log(randomNumber);
     const interval = setInterval(() => {
         $box.innerHTML = num++;
         $btn.disabled = true;
@@ -18,12 +20,3 @@ $btn.addEventListener('click', () => {
         }
     }, 5);
 });
-
-// function increased() {
-//     $box.innerHTML = num++;
-//     $btn.disabled = true;
-//     if(num === getRandomNumber()){
-//         $btn.disabled = false;
-//         clearInterval(interval);
-//     }
-// }
